8df86204971e4002589bcf4c9b3b815ef4e5ea2f
Farre4k <farhad.sherali2013@gmail.com>
2019-09-12 18:14:27 +0200

    Create Upp1.java

---
+++ src/Läxa/Upp1.java
@@ -0,0 +1,21 @@
+package Läxa;
+
+public class Upp1 {
+	
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		int i = 0;
+		
+		
+		while(i<10) {
+			
+			sum += i;
+			i++;
+			
+		}
+			
+			System.out.println(sum);
+	
+	}
+ }
