f421822b0a7d63246f9a0ced65c350c6bf43d670
Farre4k <farhad.sherali2013@gmail.com>
2019-09-15 22:28:11 +0200

    Update Upp1.java

--- src/Läxa/Upp1.java
+++ src/Läxa/Upp1.java
@@ -4,18 +4,24 @@ public class Upp1 {
 	
 	public static void main(String[] args) {
 		
-		int sum = 0;
-		int i = 0;
-		
-		
-		while(i<10) {
-			
-			sum += i;
-			i++;
-			
-		}
-			
-			System.out.println(sum);
+	int[] num = new int[] {5, 5, 1, 10, 20, 21};
+	
+	System.out.println(summering(num));
+	
 	
 	}
- }
+	
+	public static int summering(int[] num) {
+		
+		int summa = 0;
+		
+		for (int i = 0; i < num.length; i++) {
+			
+			summa += num[1];
+			
+		}
+		return summa;
+		
+	}
+
+}
